fa68df3147e2ffd7e5b8f10205f13c5c79ed41d2
Nick Pierson <nkcrpn@gmail.com>
2015-10-02 19:59:14 -0500

    Adds basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
+  require 'bcrypt'
+  include BCrypt
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+    # USER_ID and PASSWORD_HASH are included as initializers (hidden from git)
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        hash_pass = Password.new(PASSWORD_HASH)
+        user_match = username == USER_ID
+        password_match = hash_pass == password
+
+        user_match && password_match
+      end
+    end
 end
---
+++ config/initializers/auth.rb
@@ -0,0 +1,2 @@
+USER_ID       = "arlc"
+PASSWORD_HASH = "$2a$10$UewfinEkaa2rPtMfvLbGrOkIvLtm00b6tOZ7pOTPBKqi9nzFj7tY2"
