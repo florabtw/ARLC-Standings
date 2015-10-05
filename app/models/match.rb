e355ddc6790be2629e67b73b75f3619916049392
Nick Pierson <nkcrpn@gmail.com>
2015-10-04 21:04:34 -0500

    Adds match.games

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,6 +2,7 @@ class Match < ActiveRecord::Base
   belongs_to :home_team, class_name: 'Team'
   belongs_to :away_team, class_name: 'Team'
   belongs_to :match_status
+  has_many :games
 
   def to_s
     self.home_team.to_s + ' vs. ' + self.away_team.to_s
