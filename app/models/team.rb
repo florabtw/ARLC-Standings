c4a3725740a8a34e7b06d512d752602b59dc29b7
Nick Pierson <nkcrpn@gmail.com>
2015-10-04 19:34:32 -0500

    Adds team.home_matches and team.away_matches

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,9 @@ class Team < ActiveRecord::Base
   has_many :memberships
   has_many :players, through: :memberships
 
+  has_many :home_matches, class_name: 'Match', foreign_key: :home_team_id
+  has_many :away_matches, class_name: 'Match', foreign_key: :away_team_id
+
   def to_s
     self.name
   end
