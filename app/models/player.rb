54b7b211f0e6609646b9474ef617a62b59fcd4ca
Nick Pierson <nkcrpn@gmail.com>
2015-10-05 16:07:57 -0500

    Adds player standings

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,6 +3,22 @@ class Player < ActiveRecord::Base
   has_many :teams, through: :memberships
   has_many :performances
 
+  def goals
+    self.performances.reduce(0) { |sum, p| sum + p.goals }
+  end
+
+  def assists
+    self.performances.reduce(0) { |sum, p| sum + p.assists }
+  end
+
+  def saves
+    self.performances.reduce(0) { |sum, p| sum + p.saves }
+  end
+
+  def shots
+    self.performances.reduce(0) { |sum, p| sum + p.shots}
+  end
+
   def to_s
     self.username
   end
