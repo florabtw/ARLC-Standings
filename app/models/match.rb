a06a6d84c930bb7b1375f03a06972c30fc3126f6
Nick Pierson <nkcrpn@gmail.com>
2015-10-04 19:23:54 -0500

    Adds to_s for match

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,8 @@ class Match < ActiveRecord::Base
   belongs_to :home_team, class_name: 'Team'
   belongs_to :away_team, class_name: 'Team'
   belongs_to :match_status
+
+  def to_s
+    self.home_team.to_s + ' vs. ' + self.away_team.to_s
+  end
 end
