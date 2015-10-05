75366fb8740fe715fc8fe24fe2b74caf30490710
Nick Pierson <nkcrpn@gmail.com>
2015-10-05 14:51:55 -0500

    Show performances when showing game. Adds link to match from game

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,4 +1,18 @@
 class Performance < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
+
+  def to_s
+    result = ''
+    result += self.player.to_s
+    result += ': '
+    result += self.goals.to_s
+    result += ' '
+    result += self.assists.to_s
+    result += ' '
+    result += self.saves.to_s
+    result += ' '
+    result += self.shots.to_s
+    result
+  end
 end
