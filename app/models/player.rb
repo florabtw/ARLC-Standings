f6044134bd55c961b30e6bd75b1bd4838bf67e06
Nick Pierson <nkcrpn@gmail.com>
2015-10-04 19:04:31 -0500

    Adds many-to-many relations for memberships

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ActiveRecord::Base
+  has_many :memberships
+  has_many :teams, through: :memberships
+
   def to_s
     self.username
   end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,7 @@
 class Team < ActiveRecord::Base
   belongs_to :season
+  has_many :memberships
+  has_many :players, through: :memberships
 
   def to_s
     self.name
