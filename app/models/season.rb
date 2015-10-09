9e3307f2742105efb43c90c6d8d5aa240a15282b
Nick Pierson <nkcrpn@gmail.com>
2015-10-08 19:47:00 -0500

    Add order to season standings

--- app/models/season.rb
+++ app/models/season.rb
@@ -6,6 +6,10 @@ class Season < ActiveRecord::Base
     Match.all.select { |m| m if m.home_team.season == self }
   end
 
+  def ordered_teams
+    self.teams.sort_by { |t| [-t.wins, -t.goal_difference, -t.goals_for] }
+  end
+
   def meta_info
     [
       self.division.region.name,
